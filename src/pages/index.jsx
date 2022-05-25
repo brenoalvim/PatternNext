9abce107fd849adb56ef7ae01a3a2fe1080dd967
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-05-25 00:29:09 -0300

    fix bug

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
-import Meta from "../Components/Head/Meta";
-import Link from "../Components/Head/Link";
-import Title from "../Components/Head/Title";
+import Meta from "../components/Head/Meta";
+import Link from "../components/Head/Link";
+import Title from "../components/Head/Title";
 
 export default function Home() {
   return (
