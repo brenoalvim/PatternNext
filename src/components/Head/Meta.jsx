ca0f269ea90245c8bafcf4fcedfa8b3206d8146c
Breno Alvim <alvimbrenosilva6@gmail.com>
2022-07-21 18:22:31 -0300

    fix: bug in Meta.tsx (httpEquiv)

--- src/components/Head/Meta.jsx
+++ src/components/Head/Meta.jsx
@@ -5,8 +5,8 @@ export default function Meta() {
         <>
             <Head>
                 <meta charset='UTF-8' />
-                <meta http-equiv='content-language' content='pt-br' />
-                <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+                <meta httpEquiv='content-language' content='pt-br' />
+                <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
                 <meta name='viewport' content='width=device-width, initial-scale=1.0' />
                 <meta name='keywords' content='...' />
 
